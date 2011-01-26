64ed28c06750f47967e230c4feb896fc57777836
Jaydev Mahadevan <jaydevm@counsyl.com>
2011-01-26 01:05:17 -0800

    initial commit. server works.

---
+++ server.coffee
@@ -0,0 +1,45 @@
+# Node standard libraries
+sys = require "sys"
+url = require "url"
+path = require "path"
+fs = require "fs"
+
+# Third party
+require.paths.push '/usr/local/lib/node'
+express = require 'express'
+jqtpl = require 'jqtpl'
+
+# Globals
+HOST = "localhost"
+PORT = "8080"
+SITE_ROOT = process.cwd() + '/'
+
+server = express.createServer()
+
+# Configure the dev environment
+# Add all middleware options here
+server.configure 'development', ->
+  server.use express.logger()
+  # Show verbose page errors
+  server.use express.errorHandler {
+    dumpExceptions: true,
+    showStack: true}
+  # For parsing request bodies (form POSTs, etc.)
+  server.use express.bodyDecoder()
+  # Static media directory
+  server.use express.staticProvider __dirname
+
+server.set 'views', __dirname
+server.set 'view engine', 'html'
+server.register '.html', jqtpl
+server.set 'view options', {'layout': false}
+
+server.get '/', (req, res) ->
+  res.render 'home.html'
+
+server.post '/', (req, res) ->
+  console.log req.body
+  res.send()
+
+server.listen PORT, HOST
+sys.puts "Server running at #{HOST}:#{PORT}"
---
+++ server.js
@@ -0,0 +1,38 @@
+(function() {
+  var HOST, PORT, SITE_ROOT, express, fs, jqtpl, path, server, sys, url;
+  sys = require("sys");
+  url = require("url");
+  path = require("path");
+  fs = require("fs");
+  require.paths.push('/usr/local/lib/node');
+  express = require('express');
+  jqtpl = require('jqtpl');
+  HOST = "localhost";
+  PORT = "8080";
+  SITE_ROOT = process.cwd() + '/';
+  server = express.createServer();
+  server.configure('development', function() {
+    server.use(express.logger());
+    server.use(express.errorHandler({
+      dumpExceptions: true,
+      showStack: true
+    }));
+    server.use(express.bodyDecoder());
+    return server.use(express.staticProvider(__dirname));
+  });
+  server.set('views', __dirname);
+  server.set('view engine', 'html');
+  server.register('.html', jqtpl);
+  server.set('view options', {
+    'layout': false
+  });
+  server.get('/', function(req, res) {
+    return res.render('home.html');
+  });
+  server.post('/', function(req, res) {
+    console.log(req.body);
+    return res.send();
+  });
+  server.listen(PORT, HOST);
+  sys.puts("Server running at " + HOST + ":" + PORT);
+}).call(this);
