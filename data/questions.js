e9d6a4f6eff714ba5d1bf8a5b7c8cac543a69cb9
Jaydev Mahadevan <jaydevm@counsyl.com>
2011-03-05 22:06:22 -0800

    First pass at a bogus data file for loading up mongodb with test data. Eventually we'll want to import real data this way.
    To import:
      mongoimport -c <collection> <filename>

---
+++ data/questions.js
@@ -0,0 +1,3 @@
+{question: "Test Question 1", type: "free response", page_number: 292, topic: "pathology"}
+{question: "Test Question 2", type: "matching", page_number: 123, topic: "internal medicine"}
+{question: "Test Question 3", type: "free response", page_number: 839, topic: "virology"}
